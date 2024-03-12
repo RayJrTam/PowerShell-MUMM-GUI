a4eca2c502f95d2be3e56a5a0aaa6910218f89d7
Raymond Tamse Jr <raymondstevejr@yahoo.com>
2024-03-13 09:46:36 +1300

    Test commit, also re-designing UI: planning to use spreadsheet/table layout

--- MUMM.ps1
+++ MUMM.ps1
@@ -39,25 +39,6 @@ $MainForm.Height = 768
 #$MainForm.AutoSize = $true
 
 # --- Mailbox Form ---
-$MailboxLabelLocation = $StartPosition
-$MailboxLabel = New-Object System.Windows.Forms.Label
-$MailboxLabel.Text = "Enter the mailbox name or email address:"
-$MailboxLabel.Location = $MailboxLabelLocation
-$MailboxLabel.AutoSize = $true
-$MailboxLabel.Font = $Paragraph
-$MainForm.Controls.Add($MailboxLabel)
-
-<#
-To-do: You forgot to implement multiple mailboxes, it's not just for multiple users!!!!
-Enable multiline, or implement something where it lets users enter multiple
-names or email addresses in one-line, separated by ';'
-#>
-$MailboxTextbox = New-Object System.Windows.Forms.TextBox
-$MailboxTextbox.Location = Get-NewPosition -BasePosition $StartPosition -XOffset 0 -YOffset 30
-$MailboxTextbox.Width = $MainForm.ClientSize.Width - 30
-$MailboxTextbox.Font = $Paragraph
-$MainForm.Controls.Add($MailboxTextbox)
-
 $MailboxSelectBtn = New-Object System.Windows.Forms.RadioButton
 $MailboxSelectBtn.Location = Get-NewPosition $StartPosition 0 60
 $MailboxSelectBtn.Width = ($MainForm.ClientSize.Width / 2) - 30
@@ -71,47 +52,25 @@ $CalendarSelectBtn.Text = "Calendar"
 $MainForm.Controls.Add($MailboxSelectBtn)
 $MainForm.Controls.Add($CalendarSelectBtn)
 
+<#
+To-do: You forgot to implement multiple mailboxes, it's not just for multiple users!!!!
+Enable multiline, or implement something where it lets users enter multiple
+names or email addresses in one-line, separated by ';'
+#>
+$MailboxTextbox = New-Object System.Windows.Forms.TextBox
+$MailboxTextbox.Location = Get-NewPosition -BasePosition $StartPosition -XOffset 0 -YOffset 30
+$MailboxTextbox.Width = $MainForm.ClientSize.Width - 30
+$MailboxTextbox.Font = $Paragraph
+$MainForm.Controls.Add($MailboxTextbox)
+
 $MailboxSearchBtn = New-Object System.Windows.Forms.Button
 $MailboxSearchBtn.Location = Get-NewPosition $StartPosition 0 90
 $MailboxSearchBtn.Width = $MainForm.ClientSize.Width - 30
 $MailboxSearchBtn.Font = $Paragraph
-$MailboxSearchBtn.Text = "SEARCH"
+$MailboxSearchBtn.Text = "START"
 $MainForm.Controls.Add($MailboxSearchBtn)
 
-$AddUserList = New-Object System.Windows.Forms.ListView
-$AddUserList.Location = Get-NewPosition $StartPosition 0 150
-$AddUserList.Width = ($MainForm.ClientSize.Width / 2) - 30
-$AddUserList.Height = 300
-$AddUserList.Columns.Add("Users to add", ($MainForm.ClientSize.Width / 2) - 35)
-$AddUserList.MultiSelect = $true
-$AddUserList.View = "Details"
-$AddUserList.Font = $Paragraph
-$MainForm.Controls.Add($AddUserList)
 
-$AddUserListBtn = New-Object System.Windows.Forms.Button
-$AddUserListBtn.Location = Get-NewPosition $StartPosition 0 500
-$AddUserListBtn.Text = "Add"
-$AddUserListBtn.Font = $Paragraph
-$AddUserListBtn.Width = ($MainForm.ClientSize.Width / 2) - 30
-$MainForm.Controls.Add($AddUserListBtn)
-
-$RemoveUserListBtn = New-Object System.Windows.Forms.Button
-$RemoveUserListBtn.Location = Get-NewPosition $StartPosition 0 520
-$RemoveUserListBtn.Text = "Remove"
-$RemoveUserListBtn.Font = $Paragraph
-$RemoveUserListBtn.Width = ($MainForm.ClientSize.Width / 2) - 30
-$MainForm.Controls.Add($RemoveUserListBtn)
-
-$MailboxUserList = New-Object System.Windows.Forms.ListView
-$MailboxUserList.Location = Get-NewPosition $StartPosition ($MainForm.ClientSize.Width / 2) 150
-$MailboxUserList.Width = ($MainForm.ClientSize.Width / 2) - 30
-$MailboxUserList.Height = 300
-$MailboxUserList.Columns.Add("Current users", ($MainForm.ClientSize.Width / 2) - 35)
-$MailboxUserList.MultiSelect = $true
-$MailboxUserList.View = "Details"
-$MailboxUserList.Font = $Paragraph
-$MailboxUserList.AutoSize = $true
-$MainForm.Controls.Add($MailboxUserList)
 
 
 
